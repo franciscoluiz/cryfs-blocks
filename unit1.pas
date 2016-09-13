4481b130521714f9b9b9cb614f39dd7eb015611a
Francisco <franciscoluiz@gmail.com>
2016-09-13 15:18:04 -0300

    First Commit.

---
+++ unit1.pas
@@ -0,0 +1,29 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+    Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus;
+
+type
+
+    { TForm1 }
+
+    TForm1 = class(TForm)
+    private
+        { private declarations }
+    public
+        { public declarations }
+    end;
+
+var
+    Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
